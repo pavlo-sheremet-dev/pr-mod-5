8c2ca7c61cf9e06b0f493a7bd0421795bf0e99f8
OleksiiChernii <oleksii.chernii@gmail.com>
2023-01-28 12:16:36 +0200

    added functionals

--- src/api/cocktail-service.js
+++ src/api/cocktail-service.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-axios.defaults.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1';
+import axios from "axios";
+const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+axios.defaults.baseURL = "https://www.thecocktaildb.com/api/json/v1/1";
 
 const urls = Array.from({ length: 12 }, () => BASE_URL);
 
@@ -8,15 +8,32 @@ export const getTrendingCocktails = () => {
   return Promise.all(
     urls.map(async (url) => {
       const { data } = await axios.get(url);
-      return data;
+      const { idDrink, strDrinkThumb, strDrink, strGlass } = data.drinks[0];
+      return { idDrink, strDrinkThumb, strDrink, strGlass };
     })
   );
 };
 
 export const getCocktailDetail = async (id) => {
   const { data } = await axios.get(`/lookup.php?i=${id}`);
-
-  return data.drinks[0];
+  const {
+    strDrink,
+    strDrinkThumb,
+    strAlcoholic,
+    strCategory,
+    strInstructions,
+    strGlass,
+    dateModified,
+  } = data.drinks[0];
+  return {
+    strDrink,
+    strDrinkThumb,
+    strAlcoholic,
+    strCategory,
+    strInstructions,
+    strGlass,
+    dateModified,
+  };
 };
 
 export const searchByName = async (query) => {
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,41 @@
-import { CocktailsList } from '../components/CocktailsList';
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
+import { CocktailsList } from "../components/CocktailsList";
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
+import { useState, useEffect } from "react";
+import { getTrendingCocktails } from "../api/cocktail-service";
+import { toast } from 'react-toastify';
+
 
 export const Home = () => {
+  const [cocktails, setCocktails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError('');
+    getTrendingCocktails()
+      .then(setCocktails)
+      .catch(e => setError('something goes wrong'))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if(!error) return;
+    toast.error(error)
+  } , [error]);
+
   return (
     <>
       <Section>
-        <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
+        <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
           Trending cocktails
         </h1>
-
-        {/* <CocktailsList cocktails={cocktails} /> */}
+        <CocktailsList cocktails={cocktails} />
       </Section>
+      {isLoading && <Loader/>}
     </>
   );
 };
